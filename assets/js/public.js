ae656de37d5be420ecee203e071f71703d85a0c8
Jahid Hasan <jahid0676@gmail.com>
2025-10-07 18:29:14 +0600

    Update public.js

--- assets/js/public.js
+++ assets/js/public.js
@@ -2,16 +2,16 @@
 
 jQuery(document).ready(function($){
     // Pre populate email and name to booking form if user logged in
-    if( hbc_user_info ){
+    if( typeof hbc_user_info !== 'undefined' ){
         let name = hbc_user_info.username;
         let email = hbc_user_info.email;
-
-        // Fields
-        // $('#email').attr('disabled', 'disabled');
-        // $('#email').css('pointer-events', 'none');
         $('#name').val(name);
         $('#email').attr('value', email);
-        $('#email').removeAttr('required');
-        $('.tfhb-single-form label[for="email"]').text('Email');
     }
+
+    // Fields
+    // $('#email').attr('disabled', 'disabled');
+    // $('#email').css('pointer-events', 'none');
+    $('#email').removeAttr('required');
+    $('.tfhb-single-form label[for="email"]').text('Email');
 });
